021ea05cdb29948a88b93d82a15f0922bc423082
Ellen Marie Dash <me@duckie.co>
2019-05-10 11:44:58 -0700

    Add contrib/ruby-2.5 and contrib/ruby-2.6 images.

--- build_docker.bat
+++ build_docker.bat
@@ -1,2 +1,4 @@
 docker build --tag cirrusci/windowsservercore:2019 windowsservercore
 docker build --tag cirrusci/windowsservercore:cmake contrib/cmake
+docker build --tag cirrusci/windowsservercore:ruby contrib/ruby --build-arg RUBY_VERSION=2.5
+docker build --tag cirrusci/windowsservercore:ruby contrib/ruby --build-arg RUBY_VERSION=2.6
